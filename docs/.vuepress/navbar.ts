ddf3d326562a78e258199ab6c8b66eb4fac23dd9
yaoyikoudeyupy <3639929327@qq.com>
2024-12-31 14:45:52 +0800

    deploy

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -2,4 +2,12 @@ import { defineNavbarConfig } from 'vuepress-theme-plume'
 
 export const navbar = defineNavbarConfig([
   { text: '首页', link: '/' },
+  {
+    text: '节点',
+    icon: 'codicon:versions',
+    items: [
+      { text: '主节点vercel', link: 'https://docs.mengmengmao.cn/' },
+      { text: 'github', link: 'https://github.mengmengmao.cn/' },
+    ],
+  },
 ])
